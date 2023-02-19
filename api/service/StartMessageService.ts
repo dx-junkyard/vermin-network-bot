eaf1d4584066412ae09dccac9bd862c51f9fd82a
FooQoo <horseshoes.buzz.17@gmail.com>
2023-02-19 23:03:25 +0900

    メッセージを配列化する

--- api/service/StartMessageService.ts
+++ api/service/StartMessageService.ts
@@ -1,8 +1,14 @@
 import { TextMessage } from '@line/bot-sdk';
 
+const messages = [
+  '奥多摩町役場です。',
+  '獣害の通報ありがとうございます。',
+  '心よりお見舞い申し上げます。',
+];
+
 export function getStartTextMessage(): TextMessage {
   return {
     type: 'text',
-    text: '奥多摩町役場です。\n獣害の通報ありがとうございます。\n心よりお見舞い申し上げます。',
+    text: messages.join('\n'),
   };
 }
