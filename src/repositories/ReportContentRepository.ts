4a1332bb01e5e086381727b06f042b5d6529b29f
FooQoo <horseshoes.buzz.17@gmail.com>
2023-02-26 03:14:00 +0900

    数値にする

--- src/repositories/ReportContentRepository.ts
+++ src/repositories/ReportContentRepository.ts
@@ -10,12 +10,8 @@ export const convertReport = async (reportId: number): Promise<Report> => {
   const animal = JSON.parse(getElementByType('animal', reportLog))?.animal;
   const damage = JSON.parse(getElementByType('damage', reportLog))?.imageId;
   const geo = JSON.parse(getElementByType('geo', reportLog));
-  const latitude = (
-    Math.round((geo?.latitude || 0) * 1000000) / 1000000
-  ).toFixed(6);
-  const longitude = (
-    Math.round((geo?.longitude || 0) * 1000000) / 1000000
-  ).toFixed(6);
+  const latitude = Math.round((geo?.latitude || 0) * 1000000) / 1000000;
+  const longitude = Math.round((geo?.longitude || 0) * 1000000) / 1000000;
 
   return await prisma.$queryRaw`INSERT INTO ReportContent (reportId, animal, damage, geo, latitude, longitude) VALUES (${reportId}, ${animal}, ${damage}, ST_GEOMFROMTEXT(\'POINT(${longitude} ${latitude})\', 4326), ${latitude}, ${longitude})`;
 };
