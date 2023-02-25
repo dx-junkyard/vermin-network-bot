d2c12d4aba546bb2d1658f311e4d65b9d97484b3
FooQoo <horseshoes.buzz.17@gmail.com>
2023-02-26 02:49:46 +0900

    桁落とし

--- src/repositories/ReportContentRepository.ts
+++ src/repositories/ReportContentRepository.ts
@@ -10,8 +10,12 @@ export const convertReport = async (reportId: number): Promise<Report> => {
   const animal = JSON.parse(getElementByType('animal', reportLog))?.animal;
   const damage = JSON.parse(getElementByType('damage', reportLog))?.imageId;
   const geo = JSON.parse(getElementByType('geo', reportLog));
-  const latitude = geo?.latitude;
-  const longitude = geo?.longitude;
+  const latitude = (
+    Math.round((geo?.latitude || 0) * 1000000) / 1000000
+  ).toFixed(6);
+  const longitude = (
+    Math.round((geo?.longitude || 0) * 1000000) / 1000000
+  ).toFixed(6);
 
   const geomFromText = `ST_GeomFromText('POINT(${latitude} ${longitude}'), 4326)`;
   console.info(geomFromText);
