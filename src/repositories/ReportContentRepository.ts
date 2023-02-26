9bb9f5a63c0cb3992281fc90530d131e279dc05a
FooQoo <horseshoes.buzz.17@gmail.com>
2023-02-26 09:43:33 +0900

    デフォルトタイムを挿入するコードを追加

--- src/repositories/ReportContentRepository.ts
+++ src/repositories/ReportContentRepository.ts
@@ -14,7 +14,7 @@ export const convertReport = async (reportId: number): Promise<Report> => {
   const longitude = Math.round((geo?.longitude || 0) * 1000000) / 1000000;
   const point = `POINT(${latitude} ${longitude})`;
 
-  return await prisma.$queryRaw`INSERT INTO ReportContent (reportId, animal, damage, geo, latitude, longitude) VALUES (${reportId}, ${animal}, ${damage}, ST_GEOMFROMTEXT(${point}, 4326), ${latitude}, ${longitude})`;
+  return await prisma.$queryRaw`INSERT INTO ReportContent (reportId, animal, damage, geo, latitude, longitude, updatedAt) VALUES (${reportId}, ${animal}, ${damage}, ST_GEOMFROMTEXT(${point}, 4326), ${latitude}, ${longitude}, CURRENT_TIMESTAMP)`;
 };
 
 function getElementByType(type: string, array: ReportLog[]): string {
