78f540173eed9904c376b71d4a051d35d8038069
FooQoo <horseshoes.buzz.17@gmail.com>
2023-02-26 09:28:29 +0900

    文字列リテラルの展開を修正

--- src/repositories/ReportContentRepository.ts
+++ src/repositories/ReportContentRepository.ts
@@ -12,8 +12,9 @@ export const convertReport = async (reportId: number): Promise<Report> => {
   const geo = JSON.parse(getElementByType('geo', reportLog));
   const latitude = Math.round((geo?.latitude || 0) * 1000000) / 1000000;
   const longitude = Math.round((geo?.longitude || 0) * 1000000) / 1000000;
+  const point = `POINT(${longitude} ${latitude})`;
 
-  return await prisma.$queryRaw`INSERT INTO ReportContent (reportId, animal, damage, geo, latitude, longitude) VALUES (${reportId}, ${animal}, ${damage}, ST_GEOMFROMTEXT(\'POINT(${longitude} ${latitude})\', 4326), ${latitude}, ${longitude})`;
+  return await prisma.$queryRaw`INSERT INTO ReportContent (reportId, animal, damage, geo, latitude, longitude) VALUES (${reportId}, ${animal}, ${damage}, ST_GEOMFROMTEXT(${point}, 4326), ${latitude}, ${longitude})`;
 };
 
 function getElementByType(type: string, array: ReportLog[]): string {
