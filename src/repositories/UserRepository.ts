df1c1cfadc695186c5d0cca8561e2d24266d8f2d
FooQoo <horseshoes.buzz.17@gmail.com>
2023-03-04 23:12:05 +0900

    ログ追加

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -3,6 +3,7 @@ import { PrismaClient, User } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const createUser = async (userId: string): Promise<User> => {
+  console.info(`createUser: ${userId}`);
   return await prisma.user.create({
     data: {
       userId,
