78c2a2c3e9e9427a130ca9e8c116cf58dc420796
FooQoo <horseshoes.buzz.17@gmail.com>
2023-03-05 22:53:27 +0900

    ロガー追加

---
+++ src/lib/log4js/logger.ts
@@ -0,0 +1,12 @@
+import * as log4js from 'log4js';
+
+log4js.configure({
+  appenders: {
+    console: { type: 'console' },
+  },
+  categories: {
+    default: { appenders: ['console'], level: 'info' },
+  },
+});
+
+export const logger = log4js.getLogger();
