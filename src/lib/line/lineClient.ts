543f2a0b99c3dbb2a654bad011063b830d8db629
FooQoo <horseshoes.buzz.17@gmail.com>
2023-03-18 18:00:54 +0900

    環境変数のロジックを統一する

--- src/lib/line/lineClient.ts
+++ src/lib/line/lineClient.ts
@@ -1,4 +1,4 @@
-import { Client, ClientConfig } from '@line/bot-sdk';
+import { Client, ClientConfig, MiddlewareConfig } from '@line/bot-sdk';
 import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV == 'development') {
@@ -9,6 +9,12 @@ if (process.env.NODE_ENV == 'development') {
 const clientConfig: ClientConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
   channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// middleware
+export const middlewareConfig: MiddlewareConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET || '',
 };
 
 // Create a new LINE SDK client.
