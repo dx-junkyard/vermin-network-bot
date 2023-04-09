07ff8123bcb8916cab8c2831d63eea7c7cec406b
FooQoo <horseshoes.buzz.17@gmail.com>
2023-04-09 10:44:57 +0900

    修正

---
+++ src/controller/cronController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+
+import { broadcastMessage } from '../lib/line/Broadcast';
+import { pushExpireMessage } from '../lib/line/PushMessage';
+import {
+  deleteReport,
+  getExpiredReport,
+  isAllCompleteReport,
+} from '../repositories/ReportRepository';
+
+const EXPIRE_MINUTES = 30;
+
+export const cronNotice = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const isAllComplete = await isAllCompleteReport();
+
+  if (!isAllComplete) {
+    return res.status(200).json({
+      status: 'success',
+      isAllComplete,
+      notice: false,
+    });
+  }
+  const notice = await broadcastMessage();
+  return res.status(200).json({
+    status: 'success',
+    isAllComplete,
+    notice,
+  });
+};
+
+export const cronReportExpire = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const now = new Date();
+  const thresholdTime = new Date(now.getTime() - EXPIRE_MINUTES * 60 * 1000);
+  const reports = await getExpiredReport(thresholdTime);
+
+  const nonBlockingReports = reports.map(async (report) => {
+    const { userId, id } = report;
+
+    // 通報を論理削除
+    await deleteReport(id);
+
+    // 通報期限切れメッセージを送信
+    return await pushExpireMessage(userId);
+  });
+
+  await Promise.all(nonBlockingReports);
+
+  return res.status(200).json({
+    status: 'success',
+    num: reports.length,
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Import all dependencies, mostly using destructuring for better view.
 import express, { Application } from 'express';
 
-import { cronNotice, cronReportExpire } from './controller/CronController';
+import { cronNotice, cronReportExpire } from './controller/cronController';
 import { reportList } from './controller/ReportController';
 import { webhook } from './controller/WebhookController';
 import { middlewareClient } from './lib/line/LineClient';
